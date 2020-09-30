a20aa2c81650142734b9c196b13280b461e57ed6
Carlos Carvalho <carloslack@gmail.com>
2020-09-30 15:23:39 -0700

    Add ELF reader
    Demonstrate how to read from a loaded ELF
    executable via task and vma

---
+++ kelf/kelf.c
@@ -0,0 +1,94 @@
+/*
+ *  kelf.c
+ *  Carlos Carvalho
+ *  Linux ubuntu 5.4.0-42-generic
+ *
+ *  Access ELF headers from loaded/running executables
+ *
+ */
+#include <linux/module.h>
+#include <linux/pagemap.h>
+
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("Carlos Carvalho");
+
+static int my_pid;
+module_param(my_pid, int, S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH);
+
+static void read_fs(pid_t pid)
+{
+    struct vm_area_struct *vma;
+    struct page *page;
+    const char *filename;
+    void *page_addr;
+    Elf32_Ehdr* ehdr;
+    struct task_struct *tsk = get_pid_task(find_get_pid(my_pid), PIDTYPE_PID);
+
+    if(!tsk || !tsk->mm)
+        goto error_tsk;
+
+    vma = tsk->mm->mmap;
+    if (!vma)
+        goto error_vma;
+
+    if (!vma->vm_file)
+        goto error_file;
+
+    filename = vma->vm_file->f_path.dentry->d_name.name;
+
+    pr_info("[!] Retrieving Elf64 from '%s'\n", filename);
+
+    page = find_get_page(vma->vm_file->f_mapping, 0);
+    if (!page)
+        goto error_page;
+
+    page_addr = kmap_atomic(page);
+    ehdr = (Elf32_Ehdr*)page_addr;
+
+    // Parse ELF header
+    pr_info("page_addr=%p\n", ehdr);
+    pr_info("0x%02x\n", ehdr->e_ident[0]);
+    pr_info("%c\n", ehdr->e_ident[1]);
+    pr_info("%c\n", ehdr->e_ident[2]);
+    pr_info("%c\n", ehdr->e_ident[3]);
+    pr_info("type=0x%02x\n", ehdr->e_type);
+
+    // ELFCLASS32 == 1
+    // ELFCLASS64 == 2
+    pr_info("EICLASS=%d\n", ehdr->e_ident[EI_CLASS]);
+
+    kunmap_atomic(page_addr);
+    return;
+
+error_page:
+    pr_info("Error: page error\n");
+    return;
+
+error_file:
+    pr_info("Error: not a file\n");
+    return;
+
+error_vma:
+    pr_info("Error: invalid vma\n");
+    return;
+
+error_tsk:
+    pr_info("Error: failed to retrieve task for pid %d\n", pid);
+    return;
+}
+
+static int __init stat_init(void) {
+    pr_info("kelf loading...\n");
+    pr_info("Checking PID: %d\n", my_pid);
+    read_fs((pid_t)my_pid);
+
+    return 0;
+}
+
+static void __exit stat_cleanup(void) {
+    pr_info("kelf unloaded\n");
+}
+
+module_init(stat_init);
+module_exit(stat_cleanup);
+
